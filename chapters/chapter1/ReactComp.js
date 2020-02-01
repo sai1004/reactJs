87b5fbef88910f84f1a1f8b5fcc78ea354d4a4af
sai1004 <saikiran.bavandla@gmail.com>
2020-02-01 10:23:18 +0530

    commit

---
+++ chapters/chapter1/ReactComp.js
@@ -0,0 +1,14 @@
+import React { Component } from 'react';
+
+
+class ReactComp extends Component {
+
+    render() {
+        return (
+            <div> 
+
+                <h1> Hello I Am React Component! </h1>
+            </div>
+        )
+    }
+}
